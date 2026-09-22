ee885f411e0fb0602de4f712b0343feda899dbd0
Suseela Chavadhi <chavadhisuseela235@gmail.com>
2026-09-22 13:57:19 +0530

    feat: add reusable AWS VPC module

--- main.tf
+++ main.tf
@@ -7,3 +7,21 @@ locals {
     ManagedBy   = "Terraform"
   }
 }
+
+module "vpc" {
+  source = "./modules/vpc"
+
+  name               = local.project_name
+  vpc_cidr           = "10.0.0.0/16"
+  availability_zones = ["ap-south-1a", "ap-south-1b"]
+
+  public_subnet_cidrs = [
+    "10.0.1.0/24",
+    "10.0.2.0/24"
+  ]
+
+  private_subnet_cidrs = [
+    "10.0.11.0/24",
+    "10.0.12.0/24"
+  ]
+}
--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,18 @@ output "environment" {
   description = "Current deployment environment"
   value       = var.environment
 }
+
+output "vpc_id" {
+  description = "VPC ID"
+  value       = module.vpc.vpc_id
+}
+
+output "public_subnet_ids" {
+  description = "Public subnet IDs"
+  value       = module.vpc.public_subnet_ids
+}
+
+output "private_subnet_ids" {
+  description = "Private subnet IDs"
+  value       = module.vpc.private_subnet_ids
+}
